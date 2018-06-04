29146d615c1e6ffc756eaf2cbbb30130e424d9df
Kenny Becerra <KennyBecerra.j@gmail.com>
2018-06-04 00:03:04 -0700

    6/4/2018 - Updated Javascript code, added event listeners, and more debugging was completed.

--- JS/MyJS2.js
+++ JS/MyJS2.js
@@ -25,6 +25,7 @@ var BudgetController = (function () {
 		
 		Income_Records: [],
 		Expense_Records: [],
+		counter: 0,
 		
 		UpdateBudget: function() {
 			
@@ -34,42 +35,72 @@ var BudgetController = (function () {
 			for (var itr = 0; itr < this.Income_Records.length; itr++) {
 				inc_accu += (this.Income_Records[itr].Amount * 100);
 			}
-			this.Total_Income = (inc_accu / 100);
+
+			this.Income_Records.length === 0 ? this.Total_Income = 0 : this.Total_Income = (inc_accu / 100);
 	
 			for (var itr = 0; itr < this.Expense_Records.length; itr++) {
 				exp_accu += (this.Expense_Records[itr].Amount * 100);
 			}
-			this.Total_Expense = (exp_accu / 100);
+
+			this.Expense_Records.length === 0 ? this.Total_Expense = 0 : this.Total_Expense = (exp_accu / 100);
 			
-			this.Total = (inc_accu - exp_accu) / 100;
+			this.Total = Number.parseFloat(((inc_accu - exp_accu) / 100).toFixed(2));
 			
 			//this.Total_Income_Percent = Number.parseFloat(this.Total_Income/(this.Total_Income + Math.abs(this.Total_Expense))).toFixed(2) * 100;
-			this.Total_Income_Percent = Math.round((this.Total_Income /(this.Total_Income + this.Total_Expense)).toFixed(3) * 100);
-			
+
+			//this.Total_Income_Percent = Math.round((this.Total_Income /(this.Total_Income + this.Total_Expense)).toFixed(3) * 100);
+			this.Income_Records.length === 0 ? this.Total_Income_Percent = 0.00 : this.Total_Income_Percent = Number.parseFloat((Math.round((this.Total_Income/(this.Total_Income + this.Total_Expense)) * 10000) / 100).toFixed(2));
+
 			//this.Total_Expense_Percent = Number.parseFloat(Math.abs(this.Total_Expense)/(this.Total_Income + Math.abs(this.Total_Expense))).toFixed(2) * 100;
-			this.Total_Expense_Percent = Math.round((this.Total_Expense /(this.Total_Income + this.Total_Expense)).toFixed(3) * 100);
-			
+			//this.Total_Expense_Percent = Math.round((this.Total_Expense /(this.Total_Income + this.Total_Expense)).toFixed(3) * 100);
+			this.Expense_Records.length === 0 ? this.Total_Expense_Percent = 0.00 : this.Total_Expense_Percent = Number.parseFloat((Math.round((this.Total_Expense/(this.Total_Income + this.Total_Expense)) * 10000) / 100).toFixed(2));
+
 			// Update Income individual record percentages
 			for (var itr = 0; itr < this.Income_Records.length; itr++) {
-				this.Income_Records[itr].Percent = Math.round((this.Income_Records[itr].Amount / this.Total_Income).toFixed(3) * 100);
+				//this.Income_Records[itr].Percent = Math.round((this.Income_Records[itr].Amount / this.Total_Income).toFixed(3) * 100);
+				this.Income_Records[itr].Percent = Number.parseFloat((Math.round((this.Income_Records[itr].Amount / this.Total_Income) * 10000) / 100).toFixed(2));
+
 			}
 			// Update Expense individual record percentages
 			for (var itr = 0; itr < this.Expense_Records.length; itr++) {
-				this.Expense_Records[itr].Percent = Math.round((this.Expense_Records[itr].Amount / this.Total_Expense).toFixed(3) * 100);
+				//this.Expense_Records[itr].Percent = Math.round((this.Expense_Records[itr].Amount / this.Total_Expense).toFixed(3) * 100);
+				this.Expense_Records[itr].Percent = Number.parseFloat((Math.round((this.Expense_Records[itr].Amount / this.Total_Expense) * 10000) / 100).toFixed(2));
 			}
+
+			
+
+			
+
 		}
 	}
 	
 		return {
-			AddRecord: function(sign, description, amount) {
-				if (sign === "plus") {
-				Budget.Income_Records.push( new Record(sign, description, amount));
-				}
-				else {
-				Budget.Expense_Records.push( new Record(sign, description, amount));
-				}
+			Total: function () { return Budget.Total;},
+			Total_Income: function () { return Budget.Total_Income;},
+			Total_Expense: function () { return Budget.Total_Expense;},
+			Total_Income_Percent: function () { return Budget.Total_Income_Percent;},
+			Total_Expense_Percent: function () { return Budget.Total_Expense_Percent;},
+			Income_Records: function () { return Budget.Income_Records;},
+			Expense_Records: function () { return Budget.Expense_Records;},
+			AddRecord: function(sign, description, amount, id) {
+
+				sign === "plus" ? Budget.Income_Records.push( new Record(description, amount, id)) : Budget.Expense_Records.push( new Record(description, amount, id));
 				Budget.UpdateBudget();
 				
+			},
+			RemoveRecord: function (id) {
+				for (var itr = 0; itr < Budget.Income_Records.length; itr++) {
+					if (Budget.Income_Records[itr].Id === id) {
+						Budget.Income_Records.splice(itr, 1)
+					}
+				}
+				for (var itr = 0; itr < Budget.Expense_Records.length; itr++) {
+					if (Budget.Expense_Records[itr].Id === id) {
+						Budget.Expense_Records.splice(itr, 1)
+					}
+				}
+				Budget.UpdateBudget();
+
 			},
 			ShowBudget: function() {
 				console.log('********** BUDGET START ************');
@@ -78,16 +109,18 @@ var BudgetController = (function () {
 			},
 			Update: function () {
 				Budget.UpdateBudget();
-			}
+			},
+			showCounter: function () { return Budget.counter;},
+			IncrementCounter: function () {Budget.counter++; }
 		
 		
 		}
 	
 	//Records object contructor
-	function Record(sign, description, amount){
-		this.Sign = sign;
+	function Record(description, amount, id){
 		this.Description = description;
 		this.Amount = amount;
+		this.Id = id;
 		this.Percent = 0;
 	}
 	
@@ -96,6 +129,110 @@ var BudgetController = (function () {
 
 
 /* ------------  UI CONTROLLER START -----------  */
+var UIController = (function () {
+	
+	return {
+		RemoveRecord: function (my_id) {
+			var List_income = document.getElementsByClassName("income")[0].childNodes;
+			var List_expense = document.getElementsByClassName('expense')[0].childNodes;
+
+	
+			for ( var i = 0; i < List_income.length; i++) {
+				if (List_income[i].id === my_id){
+				List_income[i].parentNode.removeChild(List_income[i]);
+				}
+			}
+			for ( var i = 0; i < List_expense.length; i++) {
+				if (List_expense[i].id === my_id){
+				List_expense[i].parentNode.removeChild(List_expense[i]);
+				}
+			}
+		},
+		UpdateUI: function (total, totalIncome, totalIncomePercent, totalExpense, totalExpensePercent, IR, ER) {
+			document.getElementsByClassName('total')[0].innerText = total;
+			document.getElementsByClassName('total-income')[0].innerText = '+ ' + totalIncome;
+			document.getElementsByClassName('total-income-percent')[0].innerText = totalIncomePercent + ' %';
+			document.getElementsByClassName('total-expenses')[0].innerText = '- ' + totalExpense;
+			document.getElementsByClassName('total-expenses-percent')[0].innerText = totalExpensePercent + ' %';
+
+			for (var i = 0; i < IR.length; i++) {
+				document.getElementById(IR[i].Id).getElementsByClassName('item-percent')[0].innerText = IR[i].Percent + ' %';
+			}
+			for (var i = 0; i < ER.length; i++) {
+				document.getElementById(ER[i].Id).getElementsByClassName('item-percent')[0].innerText = ER[i].Percent + ' %';
+			}
+
+
+		},
+		AddRecord: function (sign, description, amount, id) {
+
+			mystring = ["<div class=\"row income-record\" id=\"" + id + "\">",
+			"<p class=\"item-description\">" + description + "</p>",
+			"<p class=\"item-amount\">" + amount + "</p>",
+			"<p class=\"item-percent\">%</p>",
+			"<button class=\"delete-button\"><i class=\"ion-ios-close-outline\"></i></button>",
+		  	"</div>\n"].join("\n");
+	  
+		  	mystring2 = ["<div class=\"row expenses-record\" id=\"" + id + "\">",
+			"<p class=\"item-description\">" + description + "</p>",
+			"<p class=\"item-amount\">" + amount + "</p>",
+			"<p class=\"item-percent\">%</p>",
+			"<button class=\"delete-button\"><i class=\"ion-ios-close-outline\"></i></button>",
+		 	"</div>\n"].join("\n");
+	  
+			sign === 'plus' ? document.getElementsByClassName('income')[0].innerHTML+= mystring : document.getElementsByClassName('expense')[0].innerHTML+= mystring2;
+		} 
+	}
+})();
+
+
+var controller = (function ( BudgetCtrl, UICtrl) {
+
+	var d = new Date();
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	document.getElementById("month").innerText = months[d.getMonth()];
+	document.getElementById("year").innerText = d.getFullYear();
+
+	/*
+	function Adding() {
+		var Sign = document.querySelector(".my-dropdown").value;
+		var Description = document.querySelector(".my-description").value;
+		var Amount = document.querySelector(".my-amount").value;
+		var current = BudgetCtrl.showCounter();
+		var idString = "record-"
+
+		var myId = idString + current;
+
+			BudgetCtrl.AddRecord( Sign, Description, Amount, myId);
+			UICtrl.AddRecord(Sign, Description, Amount, myId);
+			UICtrl.UpdateUI( BudgetCtrl.Total(), BudgetCtrl.Total_Income(), BudgetCtrl.Total_Income_Percent(), Budget.Total_Expense(), Budget.Total_Expense_Percent(), BudgetCtrl.Income_Records(), BudgetCtrl.Expense_Records());
+			
+	}
+	*/
+	document.querySelector('.add-button').addEventListener('click', function () {
+		var Sign = document.querySelector(".my-dropdown").value;
+		var Description = document.querySelector(".my-description").value;
+		var Amount = document.querySelector(".my-amount").value;
+		var current = BudgetCtrl.showCounter();
+		var idString = "record-"
+
+		var myId = idString + current;
+
+			BudgetCtrl.AddRecord( Sign, Description, Amount, myId);
+			UICtrl.AddRecord(Sign, Description, Amount, myId);
+			UICtrl.UpdateUI( BudgetCtrl.Total(), BudgetCtrl.Total_Income(), BudgetCtrl.Total_Income_Percent(), BudgetCtrl.Total_Expense() , BudgetCtrl.Total_Expense_Percent(), BudgetCtrl.Income_Records(), BudgetCtrl.Expense_Records());
+			BudgetCtrl.IncrementCounter();
+			
+			document.getElementById(myId).getElementsByClassName('delete-button')[0].addEventListener('click', function (){
+				var child = this.parentNode;
+				child.parentNode.removeChild(child);
+				BudgetCtrl.RemoveRecord(myId);		
+				UICtrl.UpdateUI( BudgetCtrl.Total(), BudgetCtrl.Total_Income(), BudgetCtrl.Total_Income_Percent(), BudgetCtrl.Total_Expense() , BudgetCtrl.Total_Expense_Percent(), BudgetCtrl.Income_Records(), BudgetCtrl.Expense_Records());
+			});
+			
+	});
+
+})(BudgetController, UIController);
 
 /* ------------  UI CONTROLLER END -----------  */
 
@@ -109,13 +246,48 @@ var BudgetController = (function () {
 
 
 
-
 function RemoveItem(self) {
   console.log(self);
   var child = self.parentNode;
   child.parentNode.removeChild(child);
 }
 
+function SecondTesting () {
+	var my_id = 'inc-rec-2';
+	
+	var List_income = document.getElementsByClassName("income")[0].childNodes;
+	var List_expense = document.getElementsByClassName('expense')[0].childNodes;
+
+	console.log(List_income);
+	
+	for ( var i = 0; i < List_income.length; i++) {
+		if (List_income[i].id === my_id){
+			List_income[i].parentNode.removeChild(List_income[i]);
+		}
+	}
+	for ( var i = 0; i < List_expense.length; i++) {
+		if (List_expense[i].id === my_id){
+			List_expense[i].parentNode.removeChild(List_expense[i]);
+		}
+	}
+
+}
+
+function ThirdTesting() {
+	var total = 231.43;
+	var totalIncome = 5034.34;
+	var totalIncomePercent = 23.45;
+	var totalExpense = 2345.54;
+	var totalExpensePercent = 12.86;
+
+	document.getElementsByClassName('total')[0].innerText = total;
+	document.getElementsByClassName('total-income')[0].innerText = totalIncome;
+	document.getElementsByClassName('total-income-percent')[0].innerText = totalIncomePercent + ' %';
+	document.getElementsByClassName('total-expenses')[0].innerText = totalExpense;
+	document.getElementsByClassName('total-expenses-percent')[0].innerText = totalExpensePercent + ' %';
+
+	document.getElementById('inc-rec-1').getElementsByClassName('item-percent')[0].innerText = '100 %';
+}
 
 function JustTesting() {
 	
@@ -161,5 +333,66 @@ function DeleteElement(self) {
 	console.log(Object.getPrototypeOf(self.parentElement));
 	
 }
+function FourthTest (sign, description, amount, id) {
+
+	mystring = ["<div class=\"row income-record\" id=\"" + id + "\">",
+	  "<p class=\"item-description\">" + description + "</p>",
+	  "<p class=\"item-amount\">" + amount + "</p>",
+	  "<p class=\"item-percent\">%</p>",
+	  "<button class=\"delete-button\"><i class=\"ion-ios-close-outline\"></i></button>",
+	"</div>\n"].join("\n");
+
+	mystring2 = ["<div class=\"row expenses-record\" id=\"" + id + "\">",
+	  "<p class=\"item-description\">" + description + "</p>",
+	  "<p class=\"item-amount\">" + amount + "</p>",
+	  "<p class=\"item-percent\">%</p>",
+	  "<button class=\"delete-button\"><i class=\"ion-ios-close-outline\"></i></button>",
+	"</div>\n"].join("\n");
+
+	sign === 'plus' ? document.getElementsByClassName('income')[0].innerHTML+= mystring : document.getElementsByClassName('expense')[0].innerHTML+= mystring2;
+} 
 
 
+function MyEvents(){
+	var items = Array.prototype.slice.call(document.querySelectorAll('*')).map(function(element) {
+		var listeners = getEventListeners(element);
+			return {
+				element: element,
+				Parent: element.parentNode,
+				listeners: Object.keys(listeners).map(function(k) {return { event: k, listeners: listeners[k] };
+			})
+		};
+	}).filter(function(item) {
+		return item.listeners.length;
+	});
+	
+	// See below for things you can do with the items
+	
+	// Things you can do with the items
+	
+	// 1. log them to the console
+	console.log(items);
+	
+	/*
+	// 2. Put a red border around the elements
+	items.forEach(function(item) {
+		item.element.style.outline = '1px solid red';
+	})
+	*/
+	// 3. generate a summary
+	var summary = items.map(function(item) {
+		var el = item.element,
+			id = el.id,
+			className = el.className;
+		if (className instanceof SVGAnimatedString) {
+			className = className.baseVal;
+		}
+		var str = el.tagName.toLowerCase() + (id ? '#' + id : '') + (className ? '.' + className.replace(/\s+/g, '.') : '');
+		str += ' ' + item.listeners.map(function(l) {
+			return l.event + ': ' + l.listeners.length;
+		}).join(' ');
+		return str;
+	}).join('\n');
+	
+	console.log(summary);
+}
